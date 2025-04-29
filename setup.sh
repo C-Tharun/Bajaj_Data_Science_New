0116d5bf631ac9dc1f309b2cc6170246a817cfc9
C-Tharun <116745984+C-Tharun@users.noreply.github.com>
2025-04-29 13:11:29 +0530

    Simplify setup to use system Python directly

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Create and activate virtual environment
-python -m venv venv
-source venv/bin/activate
-
 # Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
